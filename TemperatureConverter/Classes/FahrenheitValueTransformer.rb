737ee1f3c7df26871dbb172734047a6ac8638a50
Mateus <seanlilmateus@yahoo.de>
2012-01-02 13:45:25 +0100

    Update TemperatureConverter/Classes/FahrenheitValueTransformer.rb

--- TemperatureConverter/Classes/FahrenheitValueTransformer.rb
+++ TemperatureConverter/Classes/FahrenheitValueTransformer.rb
@@ -8,27 +8,27 @@
 class FahrenheitValueTransformer < NSValueTransformer
         
     def transformedValueClass
-			NSNumber
+	NSNumber
     end
     
     def allowsReverseTransformation
-			true
+	true
     end
     
     def transformedValue value
-			centigrade_from_Kelvin = NSValueTransformer.valueTransformerForName "centrigradeFromKelvin"
-			value = centigrade_from_Kelvin.transformedValue value # convert from kelvin to centigrade
-			centigrade_interim_value = value.doubleValue # handles NSString and NSNumber
-			fahrenheit_output_value = (1.8 * centigrade_interim_value) + 32
-			fahrenheit_output_value.to_f
+	centigrade_from_Kelvin = NSValueTransformer.valueTransformerForName "centrigradeFromKelvin"
+	value = centigrade_from_Kelvin.transformedValue value # convert from kelvin to centigrade
+	centigrade_interim_value = value.doubleValue # handles NSString and NSNumber
+	fahrenheit_output_value = (1.8 * centigrade_interim_value) + 32
+	fahrenheit_output_value.to_f
     end
     
     def reverseTransformedValue value
-			centigrade_fromKelvin = NSValueTransformer.valueTransformerForName "centrigradeFromKelvin"        
-			fahrenheit_input_value = value.doubleValue
-			centigrade_interim_value = (fahrenheit_input_value - 32.0) / 1.8
-			# convert from centigrade to kelvin and return....
-			centigrade_fromKelvin.reverseTransformedValue centigrade_interim_value
+	centigrade_fromKelvin = NSValueTransformer.valueTransformerForName "centrigradeFromKelvin"        
+	fahrenheit_input_value = value.doubleValue
+	centigrade_interim_value = (fahrenheit_input_value - 32.0) / 1.8
+	# convert from centigrade to kelvin and return....
+	centigrade_fromKelvin.reverseTransformedValue centigrade_interim_value
     end
 end
 
