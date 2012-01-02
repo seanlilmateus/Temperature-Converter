cc23e62cfb38f2dcc0be202c9c540c347e26d998
Mateus <seanlilmateus@yahoo.de>
2012-01-02 13:46:45 +0100

    Update TemperatureConverter/Classes/RankineValueTransformer.rb

--- TemperatureConverter/Classes/RankineValueTransformer.rb
+++ TemperatureConverter/Classes/RankineValueTransformer.rb
@@ -2,28 +2,28 @@
 #  RankineValueTransformer.rb
 #  TemperatureConverter
 #
-#  Created by Mateus Kimbango Armando on 13.05.11.
-#  Copyright 2011 Sean Coorp. INC. All rights reserved.
+#  Created by Sean Mateus  on 13.05.11.
+#  Copyright 2011 Sean Mateus.All rights reserved.
 #
 class RankineValueTransformer < NSValueTransformer
     def transformedValueClass
-			NSNumber
+	NSNumber
     end
     
     def allowsReverseTransformation
-			true
+	true
     end
     
     def transformedValue value
-			kelvin_input_value = value.doubleValue # handles NSString and NSNumber
-			rankine_output_value = kelvin_input_value * 1.8
-			rankine_output_value.to_f
+	kelvin_input_value = value.doubleValue # handles NSString and NSNumber
+	rankine_output_value = kelvin_input_value * 1.8
+	rankine_output_value.to_f
     end
     
     def reverseTransformedValue value
-			rankine_input_value = value.doubleValue # handles NSString and NSNumber
-			kelvin_ouput_value = rankine_input_value / 1.8
-			kelvin_ouput_value
+	rankine_input_value = value.doubleValue # handles NSString and NSNumber
+	kelvin_ouput_value = rankine_input_value / 1.8
+	kelvin_ouput_value
     end
 end
 
