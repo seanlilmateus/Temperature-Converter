026ec8ecb8a6d76ee13f696f027367f52632e982
Mateus <seanlilmateus@yahoo.de>
2012-01-02 13:47:05 +0100

    Update TemperatureConverter/Classes/CentigradeValueTransformer.rb

--- TemperatureConverter/Classes/CentigradeValueTransformer.rb
+++ TemperatureConverter/Classes/CentigradeValueTransformer.rb
@@ -2,8 +2,8 @@
 #  CentigradeValueTransformer.rb
 #  TemperatureConverter
 #
-#  Created by Mateus Kimbango Armando on 13.05.11.
-#  Copyright 2011 Sean Coorp. INC. All rights reserved.
+#  Created by Sean Mateus  on 13.05.11.
+#  Copyright 2011 Sean Mateus.All rights reserved.
 #
 class CentigradeValueTransformer < NSValueTransformer
     def transformedValueClass
